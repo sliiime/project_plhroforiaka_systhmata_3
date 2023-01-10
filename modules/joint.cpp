7fd9f4c2748d45d0aa8d919dcacd3b7bb40e7ff1
Kostis SirAs <kostis.siras@upstreamsystems.com>
2023-01-10 17:26:10 +0200

    modified: Joint now uses JobSequence, for testing purposes only

--- modules/joint.cpp
+++ modules/joint.cpp
@@ -58,9 +58,59 @@ Joint::Joint(Column *c1, Column *c2, jsch::JobScheduler& jobScheduler){
 
 
     Vector<Tuple> workerTuples[pm1.size];
+    auto job1 = jsch::make_job(
+        [&]{
+ // Pick the smaller partition to make the Hash Table
+            uint workerId = 0;
+
+            bool swapped = false;
+            Column *smaller, *larger;
+            Column R = pm1.GetPartition(0);
+            Column S = pm2.GetPartition(0);
+
+            if (R.num_tuples < S.num_tuples){
+                smaller = &R;
+                larger = &S;
+            } else {
+                smaller = &S;
+                larger = &R;
+                swapped = true;
+            }
+
+            // Create a hop table
+            Hoptable ht = Hoptable();
+            for (int i = 0; i < smaller->num_tuples; i++){
+                ht.insert(smaller->tuples[i].payload, smaller->tuples[i].key);
+            }
+                
+            // Iterate over the larger partition
+            for (int i = 0; i < larger->num_tuples; i++){
+
+                // Lookup and create the tuples
+                int key = larger->tuples[i].key;
+                int payload = larger->tuples[i].payload;
+
+                const Vector<uint32_t> *values = ht.lookup(payload);
+                if (values == NULL) continue;
+
+                for (uint j = 0; j < values->get_size(); j++){
+                    // Add tuples to result
+                    Tuple t;
+                    if (swapped) t = Tuple(key, values->get(j));
+                    else         t = Tuple(values->get(j), key);                    
+                    workerTuples[workerId].push(t);
+                }
+            }
+            // Set tuples to null so the destructor doesn't delete them
+            R.tuples = NULL;
+            S.tuples = NULL;
+        }
+    );
+
+    jsch::JobScheduler::JobSequence* jobSequence = jobScheduler.makeJobSequence(job1);
     // Iterate over the partitions
-    for (uint i=0; i<pm1.size; i++){
-        jobScheduler.submitJob(jsch::make_job([&,i]{
+    for (uint i=1; i<pm1.size; i++){
+        jobSequence->then(jsch::make_job([&,i]{
             // Pick the smaller partition to make the Hash Table
             uint workerId = i;
 
@@ -107,19 +157,28 @@ Joint::Joint(Column *c1, Column *c2, jsch::JobScheduler& jobScheduler){
             S.tuples = NULL;
         }));   
     }
-    jobScheduler.wait();
+
+    auto job2 = jsch::make_job(
+        [&]{
+            uint totalTuples = 0;
+            for (uint i = 0 ; i < pm1.size; i++) totalTuples += workerTuples[i].get_size();
+
+            //Initialize tuples Vector
+            this->tuples = new Vector<Tuple>(totalTuples);
+
+            //Merging tuples of each worker
+            for (uint i = 0 ; i < pm1.size; i++){
+                for (uint j = 0 ; j < workerTuples[i].get_size(); j++) this->AddTuple(workerTuples[i][j]);
+            }            
+        }
+    );
+
+    jobSequence->then(job2);
 
     //Calculate total number of tuples, so vector doesn't have to resize 
-    uint totalTuples = 0;
-    for (uint i = 0 ; i < pm1.size; i++) totalTuples += workerTuples[i].get_size();
-
-    //Initialize tuples Vector
-    this->tuples = new Vector<Tuple>(totalTuples);
-
-    //Merging tuples of each worker
-    for (uint i = 0 ; i < pm1.size; i++){
-        for (uint j = 0 ; j < workerTuples[i].get_size(); j++) this->AddTuple(workerTuples[i][j]);
-    }
+    
+    jobScheduler.submitJob(jobSequence);
+    jobScheduler.wait();
 }
 
 
