f54a1e372e28d25ba877c2fd5070452a0bffa5df
mariapar00 <mariapar492@gmail.com>
2023-01-17 21:30:22 +0200

    modified: updated docx with ccqueue & JobScheduler

--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ int main(){
     Utils::readRelations(std::cin,relations);
     
 	//Reads batch of queries and stores them to queries vector
-    std::ifstream in("workloads/small/all.work");
+    std::ifstream in("workloads/public/public.work");
     Utils::readQueryBatch(in,queries);
     /* 
     std::ifstream inPublic("workloads/public/public.work");
