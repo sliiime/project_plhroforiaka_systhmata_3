cbc1d37f21019c8ef8485c8ec790f1b27b75d7f9
mariapar00 <mariapar492@gmail.com>
2023-01-19 20:31:36 +0200

    modified: benchmarker

--- benchmarker.sh
+++ benchmarker.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 sum=0
+failed=0
 
 for ((i = 0; i < $3 ; i++)) ; 
 do 
     ./main < $1 > temp.result
     qresult=$(diff -Z temp.result $2 | tail -n +2)
 
-    duration=$(sed -e 's/.*:\(.*\) μ.*/\1/' <<<$qresult)
+    duration=$(sed -e 's/.*: \(.*\) μ.*/\1/' <<<$qresult)
+
+    re='^[0-9]+$'
+    if ! [[ $duration =~ $re ]] ; then
+        echo "error: $duration not a number" >&2; failed=$(($failed + 1))
+    fi
 
     echo $duration
     
@@ -17,4 +23,5 @@ done
 
 average=$(($sum/$3))
 
+echo "Successful runs : $(($3 - $failed))/$3" 
 echo "Average Query Calculation time : $average μ/s"
